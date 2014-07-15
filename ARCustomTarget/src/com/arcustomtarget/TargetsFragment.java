5d4cd162a984d40f7b242b992ce65ddfc02891cd
Taras Chaykivskyy <chaykivskyy.taras@gmail.com>
2014-07-15 19:51:28 +0300

    Added: - ui: create new target; - load/save data;

--- ARCustomTarget/src/com/arcustomtarget/TargetsFragment.java
+++ ARCustomTarget/src/com/arcustomtarget/TargetsFragment.java
@@ -1,5 +1,15 @@
 package com.arcustomtarget;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
@@ -21,11 +31,12 @@ import com.arcustomtarget.core.TargetsListArrayAdapter;
 import com.arcustomtarget.core.TargetsListItem;
 
 public class TargetsFragment extends Fragment {
-	private String LOGTAG = "TargetsFragment";
+	private final String LOGTAG = "TargetsFragment";
+	private final String DAT_FILE_NAME = "Targets.dat";
 	public static final String ARG_NUMBER = "menu_item_number";
 
 	private ListView _targetsListView;
-	private TargetsListItem[] _targetsList;
+	private TargetsListItem[] _targetsList = new TargetsListItem[] {};
 
 	private View _rootView;
 
@@ -34,35 +45,87 @@ public class TargetsFragment extends Fragment {
 			Bundle savedInstanceState) {
 		_rootView = inflater
 				.inflate(R.layout.second_fragment, container, false);
-		// int i = getArguments().getInt(ARG_NUMBER);
-		// String menuItemName = "Some name";
-		// getResources().getStringArray(
-		// R.array.menu_drawer_array)[i];
-		// getActivity().setTitle(menuItemName);
 
-		// targets
-		TargetsListItem item1 = new TargetsListItem("abcd", "smth.",
-				TargetsListItem.TARGET_TEXT);
-		TargetsListItem item2 = new TargetsListItem("abcd 2", "smth.",
-				TargetsListItem.TARGET_URL);
-		TargetsListItem item3 = new TargetsListItem("abcd 3", "smth.",
-				TargetsListItem.TARGET_VIDEO);
-		TargetsListItem item4 = new TargetsListItem("abcd 3", "smth.",
-				TargetsListItem.TARGET_VIDEO);
-		TargetsListItem item5 = new TargetsListItem("abcd 3", "smth.", 10);
-
-		_targetsList = new TargetsListItem[] { item1, item2, item3, item4,
-				item5 };
-
-		updateList();
-
+		_targetsListView = (ListView) _rootView.findViewById(R.id.targets_list);
 		_targetsListView.setOnItemClickListener(new TargetClickListener());
+
+		 updateList();
+
+		// Add new target
+		Button newTarget = (Button) _rootView.findViewById(R.id.target_add_new);
+		newTarget.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TargetsListItem item = new TargetsListItem("new");
+				addTarget(item);
+			}
+		});
 
 		return _rootView;
 	}
 
-	private void updateList() {
-		_targetsListView = (ListView) _rootView.findViewById(R.id.targets_list);
+	@Override
+	public void onStart() {
+		super.onStart();
+		loadTargets();
+	}
+
+	private void loadTargets() {
+		File file = new File(this.getActivity().getFilesDir(), DAT_FILE_NAME);
+		if (file.exists()) {
+			try {
+				InputStream inFile = new FileInputStream(file);
+
+				BufferedReader reader = new BufferedReader(
+						new InputStreamReader(inFile));
+				// StringBuilder out = new StringBuilder();
+				String line;
+				while ((line = reader.readLine()) != null) {
+					// out.append(line);
+					Log.i(LOGTAG, "loadTargets !!! readed from file : " + line);
+				}
+
+				reader.close();
+				inFile.close();
+			} catch (IOException e) {
+				Log.e(LOGTAG, "loadTargets IOException : " + e.getMessage());
+				e.printStackTrace();
+			}
+		} else
+			Log.e(LOGTAG, "loadTargets: file does not exists: " + DAT_FILE_NAME);
+	}
+
+	private void saveTargets() {
+		File file = new File(this.getActivity().getFilesDir(), DAT_FILE_NAME);
+		try {
+			file.createNewFile();
+		} catch (Exception e) {
+			Log.e(LOGTAG, "saveTargets exception : " + e.getMessage());
+			return;
+		}
+
+		if (file.exists()) {
+			try {
+				OutputStream outFile = new FileOutputStream(file);
+
+				String str = "Hello smb.!";
+				outFile.write(str.getBytes());
+				outFile.close();
+
+				Log.i(LOGTAG, "saveTargets !!! wrote to file : " + str);
+			} catch (FileNotFoundException e) {
+				Log.e(LOGTAG,
+						"saveTargets FileNotFoundException : " + e.getMessage());
+				e.printStackTrace();
+			} catch (IOException e) {
+				Log.e(LOGTAG, "saveTargets IOException : " + e.getMessage());
+				e.printStackTrace();
+			}
+		} else
+			Log.e(LOGTAG, "saveTargets: file does not exists: " + DAT_FILE_NAME);
+	}
+
+	private synchronized void updateList() {
 		_targetsListView.setAdapter(new TargetsListArrayAdapter(getActivity(),
 				_targetsList));
 	}
@@ -84,6 +147,20 @@ public class TargetsFragment extends Fragment {
 		updateList();
 	}
 
+	private synchronized void addTarget(TargetsListItem item) {
+		TargetsListItem[] tmp = new TargetsListItem[_targetsList.length + 1];
+
+		for (int i = 0; i < _targetsList.length; i++)
+			tmp[i] = _targetsList[i];
+		tmp[_targetsList.length] = item;
+
+		_targetsList = tmp;
+		
+		showEditDialog(_targetsList.length-1, true);
+
+		updateList();
+	}
+
 	class TargetClickListener implements ListView.OnItemClickListener {
 		@Override
 		public void onItemClick(AdapterView<?> parent, View view, int position,
@@ -92,139 +169,143 @@ public class TargetsFragment extends Fragment {
 				showInfoDialog(position);
 		}
 
-		private void showInfoDialog(int id) {
-			final int fId = id;
+	}
 
-			// custom dialog
-			Context context = getActivity();
+	private void showInfoDialog(int id) {
+		final int fId = id;
 
-			final Dialog dialog = new Dialog(context);
-			dialog.setContentView(R.layout.target_dialog_layout);
+		// custom dialog
+		Context context = getActivity();
 
-			// Caption
-			dialog.setTitle(_targetsList[id].mCaption);
+		final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.target_dialog_layout);
 
-			// Target Icon
-			ImageView image = (ImageView) dialog
-					.findViewById(R.id.targetDialogImage);
-			image.setImageResource(_targetsList[id].getDrawableId());
+		// Caption
+		dialog.setTitle(_targetsList[id].mCaption);
 
-			// Target Icon Caption
-			TextView targetIconCaption = (TextView) dialog
-					.findViewById(R.id.targetDialogImageCaption);
-			targetIconCaption.setText(_targetsList[id].getDrawableCaption());
+		// Target Icon
+		ImageView image = (ImageView) dialog
+				.findViewById(R.id.targetDialogImage);
+		image.setImageResource(_targetsList[id].getDrawableId());
 
-			// Target data text
-			TextView text = (TextView) dialog
-					.findViewById(R.id.targetDialogText);
-			text.setText(_targetsList[id].mData);
+		// Target Icon Caption
+		TextView targetIconCaption = (TextView) dialog
+				.findViewById(R.id.targetDialogImageCaption);
+		targetIconCaption.setText(_targetsList[id].getDrawableCaption());
 
-			// OK button
-			Button dialogButtonOK = (Button) dialog
-					.findViewById(R.id.targetDialogButtonOK);
-			dialogButtonOK.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					dialog.dismiss();
-				}
-			});
+		// Target data text
+		TextView text = (TextView) dialog.findViewById(R.id.targetDialogText);
+		text.setText(_targetsList[id].mData);
 
-			// Edit button
-			Button dialogButtonEdit = (Button) dialog
-					.findViewById(R.id.targetDialogButtonEdit);
-			dialogButtonEdit.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					showEditDialog(fId);
-					dialog.dismiss();
-				}
-			});
+		// OK button
+		Button dialogButtonOK = (Button) dialog
+				.findViewById(R.id.targetDialogButtonOK);
+		dialogButtonOK.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+			}
+		});
 
-			// Delete button
-			Button dialogButtonDelete = (Button) dialog
-					.findViewById(R.id.targetDialogButtonDelete);
-			dialogButtonDelete.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
+		// Edit button
+		Button dialogButtonEdit = (Button) dialog
+				.findViewById(R.id.targetDialogButtonEdit);
+		dialogButtonEdit.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				showEditDialog(fId, false);
+				dialog.dismiss();
+			}
+		});
+
+		// Delete button
+		Button dialogButtonDelete = (Button) dialog
+				.findViewById(R.id.targetDialogButtonDelete);
+		dialogButtonDelete.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				removeTargetWithId(fId);
+				dialog.dismiss();
+			}
+		});
+
+		dialog.show();
+	}
+
+	private void showEditDialog(int id, boolean newItem) {
+		final int fId = id;
+		final boolean fNewItem = newItem;
+
+		// custom dialog
+		Context context = getActivity();
+
+		final Dialog dialog = new Dialog(context);
+		dialog.setContentView(R.layout.target_edit_dialog_layout);
+
+		// Caption
+		dialog.setTitle(_targetsList[id].mCaption);
+
+		// Caption text
+		final EditText editTextCaption = (EditText) dialog
+				.findViewById(R.id.targetEditDialogTextCaption);
+		editTextCaption.setText(_targetsList[id].mCaption);
+
+		// Radio buttons
+		final RadioButton textRadioButton = (RadioButton) dialog
+				.findViewById(R.id.radioButtonText);
+		final RadioButton urlRadioButton = (RadioButton) dialog
+				.findViewById(R.id.radioButtonURL);
+		final RadioButton videoRadioButton = (RadioButton) dialog
+				.findViewById(R.id.radioButtonVideo);
+
+		if (_targetsList[id].mType == TargetsListItem.TARGET_TEXT)
+			textRadioButton.setChecked(true);
+		else if (_targetsList[id].mType == TargetsListItem.TARGET_URL)
+			urlRadioButton.setChecked(true);
+		else if (_targetsList[id].mType == TargetsListItem.TARGET_VIDEO)
+			videoRadioButton.setChecked(true);
+
+		// Data text
+		final EditText editTextData = (EditText) dialog
+				.findViewById(R.id.targetEditDialogTextData);
+		editTextData.setText(_targetsList[id].mData);
+
+		// OK button
+		Button dialogButtonOK = (Button) dialog
+				.findViewById(R.id.targetEditDialogButtonOK);
+		dialogButtonOK.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				TargetsListItem item = new TargetsListItem(editTextCaption
+						.getText().toString());
+				item.mData = editTextData.getText().toString();
+				if (textRadioButton.isChecked())
+					item.mType = TargetsListItem.TARGET_TEXT;
+				else if (urlRadioButton.isChecked())
+					item.mType = TargetsListItem.TARGET_URL;
+				else if (videoRadioButton.isChecked())
+					item.mType = TargetsListItem.TARGET_VIDEO;
+
+				changeTargetWithId(fId, item);
+
+				saveTargets();
+				dialog.dismiss();
+			}
+		});
+
+		// Cancel button
+		Button dialogButtonCancel = (Button) dialog
+				.findViewById(R.id.targetEditDialogButtonCancel);
+		dialogButtonCancel.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				dialog.dismiss();
+				if (fNewItem)
 					removeTargetWithId(fId);
-					dialog.dismiss();
-				}
-			});
+			}
+		});
 
-			dialog.show();
-		}
-
-		private void showEditDialog(int id) {
-			final int fId = id;
-
-			// custom dialog
-			Context context = getActivity();
-
-			final Dialog dialog = new Dialog(context);
-			dialog.setContentView(R.layout.target_edit_dialog_layout);
-
-			// Caption
-			dialog.setTitle(_targetsList[id].mCaption);
-
-			// Caption text
-			final EditText editTextCaption = (EditText) dialog
-					.findViewById(R.id.targetEditDialogTextCaption);
-			editTextCaption.setText(_targetsList[id].mCaption);
-
-			// Radio buttons
-			final RadioButton textRadioButton = (RadioButton) dialog
-					.findViewById(R.id.radioButtonText);
-			final RadioButton urlRadioButton = (RadioButton) dialog
-					.findViewById(R.id.radioButtonURL);
-			final RadioButton videoRadioButton = (RadioButton) dialog
-					.findViewById(R.id.radioButtonVideo);
-
-			if (_targetsList[id].mType == TargetsListItem.TARGET_TEXT)
-				textRadioButton.setChecked(true);
-			else if (_targetsList[id].mType == TargetsListItem.TARGET_URL)
-				urlRadioButton.setChecked(true);
-			else if (_targetsList[id].mType == TargetsListItem.TARGET_VIDEO)
-				videoRadioButton.setChecked(true);
-
-			// Data text
-			final EditText editTextData = (EditText) dialog
-					.findViewById(R.id.targetEditDialogTextData);
-			editTextData.setText(_targetsList[id].mData);
-
-			// OK button
-			Button dialogButtonOK = (Button) dialog
-					.findViewById(R.id.targetEditDialogButtonOK);
-			dialogButtonOK.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					TargetsListItem item = new TargetsListItem(
-							editTextCaption.getText().toString());
-					item.mData = editTextData.getText().toString();
-					if (textRadioButton.isChecked())
-						item.mType = TargetsListItem.TARGET_TEXT;
-					else if (urlRadioButton.isChecked())
-						item.mType = TargetsListItem.TARGET_URL;
-					else if (videoRadioButton.isChecked())
-						item.mType = TargetsListItem.TARGET_VIDEO;
-
-					changeTargetWithId(fId, item);
-
-					dialog.dismiss();
-				}
-			});
-
-			// Cancel button
-			Button dialogButtonCancel = (Button) dialog
-					.findViewById(R.id.targetEditDialogButtonCancel);
-			dialogButtonCancel.setOnClickListener(new OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					dialog.dismiss();
-				}
-			});
-
-			dialog.show();
-		}
+		dialog.show();
 	}
 
 }
